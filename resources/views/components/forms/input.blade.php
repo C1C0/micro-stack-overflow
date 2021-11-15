c4e69b49c211598feddfaceb5a71cf9279c8f150
C1C0 <kpskola1002@gmail.com>
2021-11-15 12:10:58 +0100

    Add errors displaying and previously submitted value

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,13 +1,21 @@
 @props(['type' => 'text', 'name', 'required' => false, 'label' => ucwords($name), 'placeholder' => ''])
 
-<div class="input-group mb-3">
+<div class="mb-3">
+    <div class="input-group ">
     <span class="input-group-text bg-white text-primary fw-bold border-1"
           id="basic-addon1">{{$label}}:</span>
-    <input name="{{ $name }}"
-           id="{{ $name }}"
-           type="{{$type}}"
-           aria-label="{{ $name }}"
-           placeholder="{{$placeholder}}"
-           class="form-control bg-white w-25"
-            {{ $required ? 'required' : '' }}>
+        <input name="{{ $name }}"
+               id="{{ $name }}"
+               type="{{$type}}"
+               aria-label="{{ $name }}"
+               placeholder="{{$placeholder}}"
+               value="{{old($name)}}"
+               class="form-control bg-white w-25"
+                {{ $required ? 'required' : '' }}>
+    </div>
+
+    @error($name)
+    <p class="text-danger">{{$message}}</p>
+    @enderror
+
 </div>
