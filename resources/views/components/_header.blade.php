2c3a53dec420e6c0f7f656da041485d38eb14732
C1C0 <kpskola1002@gmail.com>
2021-11-15 13:01:43 +0100

    Update header

--- resources/views/components/_header.blade.php
+++ resources/views/components/_header.blade.php
@@ -1,11 +1,16 @@
 <div class="d-flex justify-content-between bg-primary px-4 py-3 mb-4 rounded-bottom">
-    <x-questions.title class="text-white"><a href="/" class="text-white text-decoration-none">Micro Stack Overflow</a></x-questions.title>
+    <x-questions.title class="text-white"><a href="/" class="text-white text-decoration-none">Micro Stack Overflow</a>
+    </x-questions.title>
 
     <nav>
         <x-navigation.nav-item class="text-white">Ask Question</x-navigation.nav-item>
         @guest
             <x-navigation.nav-item class="text-white" href="/login">Log In</x-navigation.nav-item>
         @else
+            <x-navigation.nav-item class="text-white fw-bold"
+                                   href="/user/{{auth()->user()->username}}">
+                {{ auth()->user()->username }}
+            </x-navigation.nav-item>
             <x-navigation.nav-item class="text-white" href="/logout">Log Out</x-navigation.nav-item>
         @endguest
     </nav>
