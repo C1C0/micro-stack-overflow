60669835cf962da3662114865b02cc72ba287faf
C1C0 <kpskola1002@gmail.com>
2021-11-15 19:03:53 +0100

    Order by Update Question DESC

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,14 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
-    public function index(){
-        return view('question.index', ['questions' => Question::all()]);
+    public function index()
+    {
+        return view('question.index', ['questions' => Question::all()->sortByDesc('updated_at')]);
     }
 
-    public function show(Question $question){
+    public function show(Question $question)
+    {
         return view('question.show', ['question' => $question]);
+    }
+
+    public function create()
+    {
+        return view('question.create');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate(
+            [
+                'title' => ['required', 'min:5', 'max:255'],
+                'body' => ['required'],
+                'media' => ['file'],
+            ]
+        );
+
+        $attributes['user_id'] = Auth::user()->id;
+
+        $question = Question::create($attributes);
+
+        return redirect('/question/' . $question->id);
     }
 }
