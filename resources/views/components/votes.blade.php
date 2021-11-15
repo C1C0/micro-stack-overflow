89e2290a610578d55f726178c13b25449bb27ec5
C1C0 <kpskola1002@gmail.com>
2021-11-15 10:35:32 +0100

    Change votes dir

--- resources/views/components/votes.blade.php
+++ resources/views/components/votes.blade.php
@@ -1,9 +1,9 @@
 <div class="d-flex text-center text-primary align-items-center">
     <p class="me-3 mt-0 mb-0">Votes: </p>
     <div class="d-flex flex-column">
-        <x-arrow direction="up" class="m-auto"></x-arrow>
+        <x-icons.arrow direction="up" class="m-auto"></x-icons.arrow>
         <span class="fw-bold">{{$slot}}</span>
-        <x-arrow direction="down" class="m-auto"></x-arrow>
+        <x-icons.arrow direction="down" class="m-auto"></x-icons.arrow>
     </div>
 
 </div>
