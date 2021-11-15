e84221c5b73a40b7caef400d78318fed81bdb4e3
C1C0 <kpskola1002@gmail.com>
2021-11-15 12:26:45 +0100

    Fix password old value

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -4,12 +4,13 @@
     <div class="input-group ">
     <span class="input-group-text bg-white text-primary fw-bold border-1"
           id="basic-addon1">{{$label}}:</span>
+
         <input name="{{ $name }}"
                id="{{ $name }}"
                type="{{$type}}"
                aria-label="{{ $name }}"
                placeholder="{{$placeholder}}"
-               value="{{old($name)}}"
+               value="{{$type !== 'password' ? old($name) : ''}}"
                class="form-control bg-white w-25"
                 {{ $required ? 'required' : '' }}>
     </div>
