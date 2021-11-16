1a03b47aaffd363f210b756d0839d7dae7dbf16a
C1C0 <kpskola1002@gmail.com>
2021-11-16 03:17:07 +0100

    Username - unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             'users',
             function (Blueprint $table) {
                 $table->id();
-                $table->string('username');
+                $table->string('username')->unique();
                 $table->string('email')->unique();
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
